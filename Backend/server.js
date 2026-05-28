f1660169ff655d0f3b3b90551ce2236d20b75649
Rahul Kumar <rahulkumar.workspace@gmail.com>
2026-05-28 17:52:00 +0530

    what is socket io - defination

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,9 @@ import express from "express"
 import { createServer } from "http"
 import { Server } from "socket.io"
 import { YSocketIO } from "y-socket.io/dist/server"
-
+// It is a tool that locks open a permanent, instant connection between a website and a server. like a phone call bw them
+// So, Instead of constantly refreshing the page to see new data, Socket.IO keeps the line wide open so they can instantly talk to each other back and forth.
+// This lets both sides push data back and forth to each other anytime without waiting for a page refresh.
 
 const app = express()
 app.use(express.static("public"))
